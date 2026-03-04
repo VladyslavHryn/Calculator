143bf2bd1babda02421285419a23965f4584e198
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-04 22:44:16 +0200

    add stack realisation

---
+++ Stack.cs
@@ -0,0 +1,33 @@
+﻿class Stack <T>
+{
+    private T[] data = new T[100]; // масив для зберігання елементів
+    private int top = -1; // індекс верхівки стеку, -1 = стек порожній
+
+ 
+    public void Push(T value) //Додати значення в стек 
+    {
+        top++; 
+        data[top] = value; 
+        
+    }
+
+    public T Pop() // Дізнатися останнє значення зі стеку та видалити останній елемент
+    {
+        T value = data[top]; 
+        top--; 
+        return value; 
+        
+    }
+
+    public T Peek() //Дізнатися останнє значення зі стеку та не видаляти останній елемент
+    {
+        return data[top];
+        
+    }
+
+    public bool IsEmpty() //Перевірка, чи порожній стек
+    {
+        return top == -1; 
+        
+    }
+}
