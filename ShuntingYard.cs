734e55988821eedcb41bbbd4d954df15f5a77321
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-06 22:29:18 +0200

    shunting yard algorytym

---
+++ ShuntingYard.cs
@@ -0,0 +1,62 @@
+﻿class ShuntingYard
+{
+    public static string[] Convert(string[] tokens)
+    {
+        string[] output = new string[100];
+        int outCount = 0;
+        Stack<string> operators = new Stack<string>();
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            string token = tokens[i];
+
+            if (char.IsDigit(token[0]))
+            {
+                output[outCount] = token;
+                outCount++;
+            }
+            else if (token == "(")
+            {
+                operators.Push(token);
+            }
+            else if (token == ")")
+            {
+                while (operators.Peek() != "(")
+                {
+                    output[outCount] = operators.Pop();
+                    outCount++;
+                }
+                operators.Pop();
+            }
+            else if (token == "+" || token == "-" || token == "*" || token == "/")
+            {
+                while (!operators.IsEmpty() && operators.Peek() != "(" && Priority(operators.Peek()) >= Priority(token))
+                {
+                    output[outCount] = operators.Pop();
+                    outCount++;
+                }
+                operators.Push(token);
+            }
+        }
+
+        while (!operators.IsEmpty())
+        {
+            output[outCount] = operators.Pop();
+            outCount++;
+        }
+
+        string[] result = new string[outCount];
+        for (int i = 0; i < outCount; i++)
+            result[i] = output[i];
+        return result;
+    }
+
+    static int Priority(string op)
+    {
+        if (op == "*" || op == "/") 
+            return 2;
+        if (op == "+" || op == "-") 
+            return 1;
+        return 0;
+    }
+}
